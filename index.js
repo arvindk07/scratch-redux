5db308812b8b8e61c8e42525ac8b3064b853ca44
arvindk07 <info.arvind07@gmail.com>
2024-03-01 01:07:34 +0530

    reducer

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function buyCake() {
     info: "First redux application",
   };
 }
+
+// (prevState,action)=>new state
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
